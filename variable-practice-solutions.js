78a158b16c4c73328341ebb2603abb8e92d65d9a
haa-meem <haameemalhamra@gmail.com>
2025-03-17 16:38:02 +0600

    Variable Problem Solutions

---
+++ variable-practice-solutions.js
@@ -0,0 +1,21 @@
+//1
+const birthYear = 2001;
+console.log(birthYear);
+//2
+const siblings = 0;
+console.log(siblings);
+//3
+const students = 61;
+console.log(students);
+//4
+const marks = 72;
+console.log(marks);
+//5
+const rooms = 4;
+console.log(rooms);
+//6
+const studyHours = 5;
+console.log(studyHours);
+//7
+const total = 53;
+console.log(total);
