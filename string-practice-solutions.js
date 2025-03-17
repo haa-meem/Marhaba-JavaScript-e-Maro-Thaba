08ad32f696db71d450f9e97f53c682e6a6587635
haa-meem <haameemalhamra@gmail.com>
2025-03-17 19:55:33 +0600

    String Practices

---
+++ string-practice-solutions.js
@@ -0,0 +1,25 @@
+//Page 41
+
+//1
+const name = "Haa-meem Al Hamra";
+console.log(name);
+//2
+const address = "Vill & Post: Kuralia, Sirajganj Sadar";
+console.log(address);
+//3
+const bestFriendVarsity = "Uttara University";
+console.log(bestFriendVarsity);
+//4
+const fan = "Samantha Ruth Prabhu";
+console.log(fan);
+//5
+const movieOfTheYear = "N/A";
+console.log(movieOfTheYear);
+//6
+const hobby = "Fishing, Fishkeeping, Aquascaping, Watching Movies & Series, Gaming";
+//console.log(hobby);      //Vai hadn't said to show in console
+//7
+const color = "Black";
+//console.log(color);     //Vai hadn't said to show in console
+
+
