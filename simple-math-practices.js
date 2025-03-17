44a4c01ab1ccc634fa2aaab5840d1a4d791f05d7
haa-meem <haameemalhamra@gmail.com>
2025-03-17 23:15:44 +0600

    simple math practices

---
+++ simple-math-practices.js
@@ -0,0 +1,41 @@
+//Page 47-48
+
+//1
+const income = 100;
+const expenses = 40;
+console.log(income-expenses); //New Balance
+//2
+const pencil = 10;
+const atATimeUse = 2;
+console.log(pencil/atATimeUse); //I can use this times in total
+//3
+const money = 60;
+const everyTimeUse = 15;
+console.log(money/everyTimeUse); //I can spend this times in total
+console.log(money%everyTimeUse); //New Balance after total expenses
+//4
+const first = "Hello";
+const last = "World";
+console.log(first+last); //string addition
+//5
+const riceInKG = 153;
+const lentilsInKG = 261;
+console.log(riceInKG+lentilsInKG); //Total rice & lentils
+//6
+const haveTk = 500;
+const useAtATime = 75;
+console.log(haveTk/useAtATime); //I can spend money this times in total
+console.log(haveTk%useAtATime); //New Balance after total expenses
+//7
+console.log(8/3); //Quotient
+console.log(8%3); //Remainder
+//8
+console.log(50%9); //Remainder
+//9
+console.log("Bangla"+"desh");
+//10
+const eatRiceInKG = 4;
+const stock = 480;
+const totalDays = stock/eatRiceInKG;
+console.log(totalDays);
+console.log(totalDays/30); //total months
