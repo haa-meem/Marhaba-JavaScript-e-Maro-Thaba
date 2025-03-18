c52a5b86d675469c69f29e7067362414c68b2a65
haa-meem <haameemalhamra@gmail.com>
2025-03-18 18:10:03 +0600

    Comment practices

---
+++ comment-practics.js
@@ -0,0 +1,54 @@
+//Page 56
+
+//1
+//This is my VSS Weapon score in FREE FIRE, ranking 6th in Sirajganj District & 71th in Rajshahi Division
+let score = 3447;
+
+
+//2
+//This is the radius of a plate
+//It's my daily dining steel plate
+//The unit of measurement is inches
+const radius = 5.25;
+
+
+//3
+/*
+I have 3 favorite dresses.
+All of them are full shirts in different colors.
+*/
+let dress = 3;
+
+
+//4
+const giftMoney = 100;  //This money was given to me as Salami
+
+
+//5
+/*
+I have decided to study for these hours. 
+During this time, I will allocate specific hours to different subjects. 
+I will start with electrical circuits.
+Then I'll dedicate some time to coding since I am a student of Electrical & Computer Engineering. 
+Additionally, I need to study Humanities which is a common subject for all Engineering students.
+*/
+const studyTime = 7;
+
+
+//6
+/*
+I sent this amount of money to my friend, Shabbir.
+Out of this, 500tk was the amount I borrowed from him and 9.25tk was the cashout charge of Bkash.
+*/
+const sendMoney = 509.25;
+
+
+//7
+/*
+I preordered the book "Marhaba, Javascript e Maro Thaba" from Rokomari.com. 
+Initially, the book's price was 780tk, but after the preorder discount, the price decreased to 585tk. 
+I had to pay an extra 48tk as Online Platform Charges. 
+However, when I received the book, the total cost was 645tk as I chose Cash on Delivery. 
+Since I didn't have that amount of cash in hand, I sent the money along with the Bkash cashout charge to the Courier Brother.
+*/
+const pay = 645;
