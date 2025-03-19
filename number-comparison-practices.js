1fa02d9ea8e8110c880bdcab60f5e2860e9d2dc2
haa-meem <haameemalhamra@gmail.com>
2025-03-19 16:46:13 +0600

    Number Comparison Practices

---
+++ number-comparison-practices.js
@@ -0,0 +1,22 @@
+//Page 68
+
+//1
+console.log(7>5);   //I ate 7 sandwiches & my younger brother ate 5 sandwiches
+
+//2
+console.log(45<50);   //There are 45 students in my class & 50 in the class next to mine
+
+//3
+console.log(75==75);    //My friend & I both scored 75 marks on the exam
+
+//4
+console.log(95>=100);   //My target is 100 taka & I have gathered 95 taka
+
+//5
+console.log(10!=7);   //Different
+
+//6
+console.log(20<=15);    //less than or equal
+
+//7
+console.log(45>97);   //I scored 45 on the exam & my friend scored 97
