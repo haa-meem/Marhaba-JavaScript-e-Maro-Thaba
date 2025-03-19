666601192a9027b355b8cacd81ce464041145b41
haa-meem <haameemalhamra@gmail.com>
2025-03-19 12:17:27 +0600

    Naming Practices

---
+++ naming-practices.js
@@ -0,0 +1,28 @@
+//Page 59
+
+//1
+const favFood = "Biryani";
+
+//2
+const city = "Chittagong";
+const City = "Dhaka";
+console.log(city, City);
+
+//3
+const userName = "Johan";
+const username = 'Jovan';
+console.log(userName, username);
+
+//4
+const myHomeAddress = "Sirajganj";
+console.log(myHomeAddress);
+
+//5
+//World Cup 2019 Super over scorecard of ENG & NZ
+//const super = 15;   //super is a JavaScript keyword & used in object-oriented programming
+
+//6
+//const class = "BSc 3rd Year Even Semester";   //class is a JS template for creating objects & helps implement OOP
+
+//7
+//const 2ndPosition = "Najmul Hossain Shanto";  //naming can't be start with Number
