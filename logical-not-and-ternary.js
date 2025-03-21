844d0ef3d0604dec1246a970a984f8064128afd6
haa-meem <haameemalhamra@gmail.com>
2025-03-21 23:00:48 +0600

    Logical NOT & Ternary operator practice

---
+++ logical-not-and-ternary.js
@@ -0,0 +1,34 @@
+//Page 90
+
+//1
+let buy = 3100;
+let cashback = buy > 3000 ? 500 : 0;
+console.log(cashback);
+
+//2
+let age = 23;
+age > 15 ? console.log("Teenager") : console.log("Child");
+
+//3
+let isLoggedIn = true;
+isLoggedIn == true ? console.log("Welcome back") : console.log("Please login");
+
+//4
+let oilTankFUll = true;
+oilTankFUll == true ? console.log("Ready for a log drive") : console.log("Fill the tank");
+
+//5
+let isPassed = true;
+isPassed == true ? console.log("Party time") : console.log("Next Semester e serious study korbo");
+
+//6
+const sunny = false;
+if (!sunny) {
+    console.log("Stay home");
+}
+
+//7
+const expensive = false;
+if (!expensive) {
+    console.log("I will buy this item")
+}
