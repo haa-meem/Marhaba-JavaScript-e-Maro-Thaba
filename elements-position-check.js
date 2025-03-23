5ccdedeb9e5788d23bb48a30f257866c16387370
haa-meem <haameemalhamra@gmail.com>
2025-03-23 06:00:20 +0600

    Elements position check

---
+++ elements-position-check.js
@@ -0,0 +1,31 @@
+//Page 96
+
+//1
+const friends = ['Shabbir', 'Manik', 'Topu', 'Shanto', 'Galib'];
+console.log(friends[3]);
+
+//2
+const books = ['Pather Panchali', 'Chander Pahar', 'Devdas', 'Durgeshnandini', 'Kapalkundala', 'Bisher Dhoya', 'Carmilla'];
+console.log(books[5]);
+
+//3
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers[7] = 30;
+console.log(numbers);
+
+//4
+const games = ['Free Fire', 'PUBG', 'Candy Crush', 'Temple Run'];
+games[2] = 'Subway Surfers';
+console.log(games);
+
+//5
+const family = ['Baba', 'Ammu', 'Bubu', 'Boroma', 'Kakku', 'Bhabi', 'Bhatija'];
+console.log(family[3]);
+
+//6
+const countries = ['Saudi Arabia', 'Switzerland', 'Japan', 'Italy', 'New Zealand', 'Egypt', 'Australia'];
+console.log(countries[4]);
+
+//7
+const readingTable = ['Laptop', 'Notebook', 'Pen', 'Pencil'];
+console.log(readingTable[7]);
