f098a93570bcc4295897dc2f6626c69a40263e06
haa-meem <haameemalhamra@gmail.com>
2025-03-24 03:58:46 +0600

    While loop practice

---
+++ while-loop.js
@@ -0,0 +1,37 @@
+//Page 109
+
+//1
+let i = 0;
+while (i < 10) {
+    console.log("I'll practice Programming minimum 3 hours everyday");
+    i += 1;
+}
+
+//2
+let j = 1;
+while (j <= 20) {
+    console.log(j);
+    j += 1;
+}
+
+//3
+let k = 50;
+while (k <= 100) {
+    console.log(k);
+    k += 1;
+}
+
+//4
+let mul = 1;
+while (mul <= 10) {
+    console.log(mul * 3);
+    mul += 1;
+}
+
+//5
+//I didn't understand the problem, the loop could never run because of the condition
+let divi = 111;
+while (divi <= 110) {
+    console.log(divi / 2);
+    divi++;
+}
