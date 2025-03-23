c2c80ef138343657fe7b1aa72793d8fa52ede840
haa-meem <haameemalhamra@gmail.com>
2025-03-24 04:03:51 +0600

    While loop sum  practice

---
+++ while-loop-sum.js
@@ -0,0 +1,42 @@
+//Page 111
+
+//1
+let i = 50;
+while (i <= 100) {
+    console.log(i);
+    i += 1;
+}
+
+//2
+let j = 5, sum = 0;
+while (j <= 15) {
+    sum += j;
+    j++;
+}
+console.log('Sum of 5 to 15:', sum);
+
+//3
+let studentRoll = 1, rollSum = 0;
+while (studentRoll <= 50) {
+    console.log('Roll:', studentRoll);
+    rollSum += studentRoll;
+    studentRoll += 1;
+}
+console.log('Sum of Student roll from 1 to 50:', rollSum);
+
+//4
+let k = 21, sumNum = 0;
+while (k <= 50) {
+    sumNum += k;
+    console.log(sumNum);
+    k++;
+}
+
+
+//5
+let n = 20, sumN = 0;
+while (n <= 40) {
+    sumN += n;
+    n++;
+}
+console.log('Sum of 20 to 40:', sumN);
