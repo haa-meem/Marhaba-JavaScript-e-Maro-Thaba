3f70ebc02e4bd45b7584145ed9f83c9c538bc93b
haa-meem <haameemalhamra@gmail.com>
2025-04-18 23:59:46 +0600

    Odds numbers found or not

--- logic-build-up-3.js
+++ logic-build-up-3.js
@@ -30,3 +30,17 @@ function doubleOdds(numbers) {
 }
 console.log(doubleOdds([1, 4, 2, 5, 7, 9, 6]));
 console.log(doubleOdds([11, 34, 6, 39, 29, 13]));
+
+//3
+function oddsOrEvens(numbers) {
+    for (const number of numbers) {
+        if (number % 2 === 1) {
+            return "Odd numbers found";
+        } else {
+            return "No odd numbers found";
+        }
+    }
+}
+console.log(oddsOrEvens([11, 13, 15, 2, 32, 96, 5]));
+console.log(oddsOrEvens([11, 13, 15, 17, 19, 21]));
+console.log(oddsOrEvens([12, 14, 16, 18, 20, 22]));
