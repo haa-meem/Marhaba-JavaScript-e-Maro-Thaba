c64358705c27f4a5f407074cb6688e13d6b3134a
haa-meem <haameemalhamra@gmail.com>
2025-04-21 23:56:32 +0600

    Default parameter of income() function

--- parameter-default-value.js
+++ parameter-default-value.js
@@ -8,7 +8,13 @@ function add(num1 = 0, num2 = 0, num3 = 0) {
 console.log(add());
 
 //2
-function moneyDeposit(deposit=50){
+function moneyDeposit(deposit = 50) {
     return deposit;
 }
 console.log(moneyDeposit(100));
+
+//3
+function income(name = "anonymous", monthly = 0) {
+    console.log(name, monthly);
+}
+income("Haa-meem", 60000);
