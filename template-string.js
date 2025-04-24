82205e18f76a9c2150e97b3292a564379126bd93
haa-meem <haameemalhamra@gmail.com>
2025-04-24 19:28:41 +0600

    Employee name,age,salary show using Template string

--- template-string.js
+++ template-string.js
@@ -3,3 +3,7 @@
 //1
 const a = 100, b = 20;
 console.log(`The difference between ${a} and ${b} is ${a - b}`);
+
+//2
+const employee = { name: "MD. ABDUL ALIM", age: 60, salary: 50000 };
+console.log(`Name: ${employee.name}, Age: ${employee.age}, Salary: ${employee.salary}`)
