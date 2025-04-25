e6836a38c0f92e55c0fbb71eabbf7ef9d750bf7a
haa-meem <haameemalhamra@gmail.com>
2025-04-26 01:11:00 +0600

    Arrays first and last index add by arrow function

--- arrow-function.js
+++ arrow-function.js
@@ -15,3 +15,7 @@ console.log(arrow());
 //4
 const object = (obj) => obj.money / 5;
 console.log(object({ name: "Haa-meem", money: 350 }));
+
+//5
+const arrLength = (arr) => arr[arr.length - 1] + arr[0];
+console.log(arrLength([15, 7, 11, 13]));
