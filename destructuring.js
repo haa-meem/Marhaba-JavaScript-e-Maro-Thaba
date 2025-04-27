4e14a1ccada51e57950ddc49ccfdefb281131b00
haa-meem <haameemalhamra@gmail.com>
2025-04-27 23:30:14 +0600

    object destructuring 2

--- destructuring.js
+++ destructuring.js
@@ -4,3 +4,8 @@
 const product = { name: "Laptop", price: 50000, brand: "Dell" };
 const { brand } = product;
 console.log(brand);
+
+//2
+const item = { name: "Mobile", price: 20000, phone: "Samsung" };
+const { phone, price } = item;
+console.log(phone, price);
