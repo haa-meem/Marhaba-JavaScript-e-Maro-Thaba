9bec949896bba8c022c9617b12736f9b05b803a2
Haa-meem <haameemalhamra@gmail.com>
2025-04-29 21:39:44 +0600

    Rest operator in another object

--- rest-operator.js
+++ rest-operator.js
@@ -4,3 +4,8 @@
 const product = { name: "Laptop", price: 50000, brand: "Dell" };
 const { name, ...details } = product;
 console.log(details);
+
+//2
+const project = { id: 101, title: "Web App", budget: 3000, client: "Tech Corp" };
+const { title, ...others } = project;
+console.log(others);
