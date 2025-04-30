9e9f76409ec342cabf64267aa6cb80948eb2d31c
haa-meem <haameemalhamra@gmail.com>
2025-04-30 23:50:22 +0600

    Spread operator in Array

--- spread-operator.js
+++ spread-operator.js
@@ -5,3 +5,8 @@ const technologies = ["Condition", "array", "loop"];
 const newArray = ["variable", ...technologies];
 console.log(newArray);
 console.log(technologies);
+
+//2
+const fruits = ["Apple", "Banana", "Mango"];
+const allFruits = [...fruits, "papaya", "orange"];
+console.log(allFruits);
