c36e673209c58c2abf877f39b605410c116745cd
haa-meem <haameemalhamra@gmail.com>
2025-05-02 00:31:12 +0600

    Height filtering from array

--- map-foreach-filter-find.js
+++ map-foreach-filter-find.js
@@ -15,6 +15,11 @@ const number = [10, 15, 20, 25, 30, 35];
 const upperTwenty = number.find(num => num > 20);
 console.log(upperTwenty);
 
+//4
+const heights = [65, 70, 68, 72, 68, 73];
+const taller = heights.filter(height => height > 69);
+console.log(taller);
+
 //5
 const numbers = [7, 10, 15, 20, 25, 30];
 const dividedByThree = numbers.map(num => num / 3);
