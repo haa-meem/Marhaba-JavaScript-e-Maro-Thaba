864d5f1e69affc80058696dd9a1a69ff1ec24a69
haa-meem <haameemalhamra@gmail.com>
2025-05-01 21:30:16 +0600

    Array filtering

--- map-foreach-filter-find.js
+++ map-foreach-filter-find.js
@@ -4,3 +4,8 @@
 const snackPrice = [30, 45, 20, 60, 10];
 const incPrice = snackPrice.map(price => price + 13);
 console.log(incPrice);
+
+//2
+const legendPlayers = ["Messi", "Maradona", "Pele", "Zidane", "Ronaldo"];
+const players = legendPlayers.filter(player => player.length > 5);
+console.log(players);
