ae4864dcf5a4707d3abeaf86be2a63550231496a
haa-meem <haameemalhamra@gmail.com>
2025-05-01 20:54:39 +0600

    Knowing about module, export and import

---
+++ export-import.js
@@ -0,0 +1,23 @@
+//Page 214
+
+//1
+/* By "export", I can send specific function,variable or others for using from outside(any file/folder)
+ And by "import", that specific function,variable or others can be accessed */
+
+//2
+/* "default export" is different from "export".
+ If "default export" is used, I can "import" it by any name I wish as there only one "default export" can be used in one file */
+
+//3
+/* I can use "as" at the time of "import" and change name of that which is imported.
+ It is used in that {} and at f"irst t"hat name which is imported and then "as" and then new name of that */
+
+//4
+/* "export" is normal case because I can export multiple function, variable or others from one file and at the time of "import",
+ I have to use {as} for changing exported things name.
+ But when I'll use "default export", I don't have to worry about imported things changing name
+ because I can export only one thing from one file. */
+
+//5
+/* No, It's not possible to use more than one "default export" in the same file 
+ but I can use "export", known as "Named Export" in the same file */
