196dde5ede4e10db47659c154b5f95dd2ae9e7ff
haa-meem <haameemalhamra@gmail.com>
2025-05-03 00:18:31 +0600

    some in array searching string

--- some-every-flat.js
+++ some-every-flat.js
@@ -8,3 +8,8 @@ console.log(aboveCentury);
 //2
 const dividedByFive = [11, 13, 7, 9, 4, 100, 2001].every(num => num % 5 === 0);
 console.log(dividedByFive);
+
+//3
+const words = ["hola", "arigato", "hello"];
+const equalHello = words.some(word => word === "hello");
+console.log(equalHello);
