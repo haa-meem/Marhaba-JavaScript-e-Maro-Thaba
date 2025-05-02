33bef35f6d208c7ca18a29eed2a87436530d8df1
haa-meem <haameemalhamra@gmail.com>
2025-05-03 00:17:36 +0600

    every in array

--- some-every-flat.js
+++ some-every-flat.js
@@ -4,3 +4,7 @@
 const numbers = [11, 13, 7, 9, 4, 99, 2001];
 const aboveCentury = numbers.some(number => number > 100);
 console.log(aboveCentury);
+
+//2
+const dividedByFive = [11, 13, 7, 9, 4, 100, 2001].every(num => num % 5 === 0);
+console.log(dividedByFive);
