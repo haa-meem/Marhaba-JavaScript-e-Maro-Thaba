41252d52e5eb347980e2a4d5c21d36cd7f8827d1
haa-meem <haameemalhamra@gmail.com>
2025-05-04 20:00:54 +0600

    prices in total from array of objects  by reduce

--- reduce.js
+++ reduce.js
@@ -4,3 +4,8 @@
 const nums = [5, 10, 15, 20, 25];
 const sum = nums.reduce((accumulator, current) => accumulator + current, 0);
 console.log(sum);
+
+//2
+const products = [{ name: 'shampoo', price: 100 }, { name: 'soap', price: 50 }, { name: 'ttothpaste', price: 75 }];
+const totalPrice = products.reduce((accumulator, current) => accumulator + current.price, 0);
+console.log(totalPrice);
