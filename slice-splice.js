c9f0a617941c93b278477b3522afa68059b0be60
haa-meem <haameemalhamra@gmail.com>
2025-05-12 18:21:55 +0600

    array splicing

--- slice-splice.js
+++ slice-splice.js
@@ -5,5 +5,10 @@ const fruits = ["Apple", "Banana", "Cherry", "Date"];
 console.log(fruits.slice(1, 3));
 
 //2
-const cars=["Tesla","BMW","Toyota","Ford"];
-console.log(cars.slice(0,2));
+const cars = ["Tesla", "BMW", "Toyota", "Ford"];
+console.log(cars.slice(0, 2));
+
+//3
+const movieList = ["Inception", "Titanic", "Joker", "Avatar", "Interstellar"];
+console.log(movieList.splice(2, 1, "Batman", "Superman"));
+console.log(movieList);
